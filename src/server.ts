5246eb650f6292292fe2f2d7f2a6eac2408b9f91
Yuly Rauen <rauenyuly@gmail.com>
2025-01-07 15:20:11 -0300

    Registering fastify

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,31 @@
 import fastify from "fastify";
 import fastifyCors from "@fastify/cors";
+import {
+  validatorCompiler,
+  serializerCompiler,
+} from "fastify-type-provider-zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 
 const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, { origin: "*" });
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Typed API",
+      version: "1.0.0",
+    },
+  },
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
 
 app.listen({ port: 3000 }).then(() => {
   console.log("Server Running!");
