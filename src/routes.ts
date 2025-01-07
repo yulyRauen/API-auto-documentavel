a6c24e96b11beccd82e99544901d100d93da8bb8
Yuly Rauen <rauenyuly@gmail.com>
2025-01-07 16:07:22 -0300

    Create User Interface

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,15 @@
 import { FastifyInstance } from "fastify";
 import z from "zod";
 import { FastifyTypedInstance } from "./types";
+import { randomUUID } from "node:crypto";
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const users: User[] = [];
 
 export async function routes(app: FastifyTypedInstance) {
   app.get(
@@ -28,10 +37,16 @@ export async function routes(app: FastifyTypedInstance) {
         }),
       },
     },
-    (request) => {
+    (request, reply) => {
       const { name, email } = request.body;
 
-      return {};
+      users.push({
+        id: randomUUID(),
+        name,
+        email,
+      });
+
+      return reply.status(201).send();
     }
   );
 }
